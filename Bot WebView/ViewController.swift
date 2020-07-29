190d8ecab433d6008b89cc8b389aa782cc813760
Rahadian <rahadian@vouch.sg>
2020-07-29 10:06:24 +0700

    Initial Commit

---
+++ Bot WebView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bot WebView
+//
+//  Created by Rahadian Ahmad on 29/07/20.
+//  Copyright © 2020 vouch.sg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
