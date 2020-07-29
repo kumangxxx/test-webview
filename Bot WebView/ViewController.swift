ed53a53677a598aba31fdb974c2fef6526bf8e2b
Rahadian <rahadian@vouch.sg>
2020-07-29 11:59:42 +0700

    wkwkwkwk

--- Bot WebView/ViewController.swift
+++ Bot WebView/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
 
+    @IBOutlet weak var webview: WKWebView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        webview?.navigationDelegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        print("did appear!")
+        
+        let url = URL(string:"https://google.com")
+        let req = URLRequest(url: url!)
+        let nav = webview?.load(req)
+    }
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        print("start")
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        print("fail")
     }
 
 
